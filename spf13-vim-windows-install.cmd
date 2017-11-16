d69e8cd9f2ea0badbb885de1d22add671bc06853
michaellincn <michaellincn@users.noreply.github.com>
2017-11-16 22:39:09 +0800

    Update spf13-vim-windows-install.cmd
    update git addresss

--- spf13-vim-windows-install.cmd
+++ spf13-vim-windows-install.cmd
@@ -16,9 +16,9 @@ REM    limitations under the License.
 @if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
-@set APP_PATH=%HOME%\.spf13-vim-3
+@set APP_PATH=%HOME%\.spf13-vim-3-mlc
 IF NOT EXIST "%APP_PATH%" (
-    call git clone -b 3.0 https://github.com/spf13/spf13-vim.git "%APP_PATH%"
+    call git clone -b 3.0 https://github.com/michaellincn/spf13-vim.git "%APP_PATH%"
 ) ELSE (
     @set ORIGINAL_DIR=%CD%
     echo updating spf13-vim
